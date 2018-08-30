9dacf162454373d6c02b1a06c93d24f569f1ab08
sunnyangee <hwagae2003@gmail.com>
2018-08-30 15:52:40 +0900

    할머니 정보 보기 구축 완료

--- halae-server/routes/halmae/info.js
+++ halae-server/routes/halmae/info.js
@@ -8,7 +8,8 @@ const jwt = require('../../module/jwt.js');
 
 //파라미터로 할머니 idx 
 //halmate 테이블에서 할머니 정보(이름, 성별, 주소, 번호)  +  volunteer 테이블에서 할머니 idx 에 봉사자 count 
-router.post('/', async (req, res) => {
+
+router.get('/:hal_idx', async(req, res) => {
 
   if(!req.params.hal_idx){
     res.status(403).send({
@@ -19,13 +20,17 @@ router.post('/', async (req, res) => {
   }
 
   try{
-    let infoResultData = new Object();
+    let infoResultData = {};
 
     //할머니 정보 가져오기 -> 할머니 이름, 성별, 프사, 주소, 번호 
     let gethalinfo_Query = 'SELECT hal_img, hal_name, hal_gender, hal_address, hal_phone FROM HalAe.halmate WHERE hal_idx = ?';
-    let halinfo_Result = await db.queryParamCnt_Arr(gethalinfo_Query, [req.params.hal_idx]);
+    let halinfo_Result = await db.queryParam_Arr(gethalinfo_Query, [req.params.hal_idx]);
 
-    console.log(halinfo_Result);
+    infoResultData.hal_name = halinfo_Result[0].hal_name;
+    infoResultData.hal_gender = halinfo_Result[0].hal_gender;
+    infoResultData.hal_img = halinfo_Result[0].hal_img;
+    infoResultData.hal_address = halinfo_Result[0].hal_address;
+    infoResultData.hal_phone = halinfo_Result[0].hal_phone;
 
     if(!halinfo_Result){
       res.status(300).send({
@@ -36,12 +41,18 @@ router.post('/', async (req, res) => {
     }
 
     //할머니 봉사자 수 가져오기
-    let countvol_Query = 'SELECT COUNT(*) FROM HalAe.volunteer WHERE hal_idx = ?';
-    let countvol_Result = await db.queryParamCnt_Arr(countvol_Query, [req.params.hal_idx]);
+    let countvol_Query = 'SELECT COUNT(*) AS vol_cnt FROM HalAe.volunteer WHERE hal_idx = ?';
+    let countvol_Result = await db.queryParam_Arr(countvol_Query, [req.params.hal_idx]);
 
-    console.log(countvol_Result);
+    infoResultData.vol_cnt = countvol_Result[0].vol_cnt;
 
-    
+    console.log(infoResultData);
+
+    res.status(201).send({
+      "message" : "get halmate information Success",
+      result : infoResultData
+    });
+
 
   }catch(err){
     console.log(err);
